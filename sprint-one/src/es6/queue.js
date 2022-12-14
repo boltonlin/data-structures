d1102422d8949381f68cf4d17fd4992f570659b9
boltonlin <boltonlinbo@gmail.com>
2022-12-14 14:03:45 -0800

    implemented es6 queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,23 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    if (this.head === undefined) {
+      this.head = 0;
+    }
+    this.storage[this.size()] = value;
+  }
+
+  dequeue() {
+    var output = this.storage[this.head];
+    this.head += 1;
+    return output;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
