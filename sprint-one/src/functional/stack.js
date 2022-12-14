52338076c840a4c8e40d93f89ba8729cb2663af9
boltonlin <boltonlinbo@gmail.com>
2022-12-14 11:59:01 -0800

    functional stack implemented

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,23 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
+    var output = storage[someInstance.size() - 1];
+    delete storage[someInstance.size() - 1];
+    return output;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
