f9ebec1784d5c13486de9ff9f54be1779b7286bb
boltonlin <boltonlinbo@gmail.com>
2022-12-14 12:14:08 -0800

    implemented functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,20 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (someInstance.head === undefined) {
+      someInstance.head = 0;
+    }
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    var output = storage[someInstance.head];
+    someInstance.head += 1;
+    return output;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
